d41a59941007868d47e58702f981becf56acfd6a
SagaOfAGuy <bandoodle125@gmail.com>
2024-01-02 20:37:48 -0500

    Updated server code

--- utils.js
+++ utils.js
@@ -1,49 +1,18 @@
-import * as fs from 'fs';
-
-// Fundtion to wait until video file has finished downloading. Give it an hour timeout and check every second. 
-export async function waitForFile(filePath, pollingInterval = 1000, timeout = 600000) {
-      let elapsedTime = 0;
-      let previousSize = -1;
-      return new Promise((resolve, reject) => {
-        const intervalId = setInterval(async () => {
-          try {
-            const stats = await fs.promises.stat(filePath);
-            const currentSize = stats.size;
-    
-            if (currentSize === previousSize && currentSize > 0) {
-              // File size hasn't changed, and it's not an empty file, assuming download is complete
-              console.log('File download complete!'); 
-              clearInterval(intervalId);
-              resolve(true);
-            } else {
-              console.log(`File size for ${filePath}`, currentSize); 
-              previousSize = currentSize;
-              
-              // Check for timeout
-              elapsedTime += pollingInterval;
-              if (elapsedTime >= timeout) {
-                console.log('File download timeout!');
-                clearInterval(intervalId);
-                resolve(false);
-              }
-            }
-          } catch (error) {
-            // Handle errors (e.g., file does not exist)
-            console.error('Error:', error);
-            clearInterval(intervalId);
-            resolve(false);
-          }
-        }, pollingInterval);
-      });
-}
+import ytdl from 'ytdl-core'
 
 
-// Function to read file buffer
-export async function readFileToBuffer(filePath) {
+// Function to generate download link
+export async function getDownloadLink(url) {
   try {
-    const buffer = await fs.promises.readFile(filePath);
-    return buffer;
-  } catch (err) {
-    throw err;
+      let info = await ytdl.getInfo(url);
+      let videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highestvideo', filter: 'audioandvideo' });
+      const result = {
+        downloadUrl: videoFormat.url,
+        videoInfo: info
+      }
+      return result; 
+  } catch(error) {
+      console.error('Error', error);
+      throw error; 
   }
 }
